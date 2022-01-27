9c6955d8e86eef6e0b0f490e24a6f8e17cbebf1d
marcosasilva104 <marcosasilva104@gmail.com>
2022-01-27 16:12:00 -0300

    Developed person created

---
+++ src/main/java/com/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,27 @@
+package com.digitalinnovation.personapi.service;
+
+import com.digitalinnovation.personapi.dto.MessageResponseDTO;
+import com.digitalinnovation.personapi.entity.Person;
+import com.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person) {
+        Person savePerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with id " + savePerson.getId())
+                .build();
+    }
+}
